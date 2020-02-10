d9daa7ee70c28a7f82a19679eb625f9939122365
Yann-arthur <Speddy@localhost.localdomain>
2020-02-10 12:02:06 +0100

    [add] ia

---
+++ Marta.py
@@ -0,0 +1,10 @@
+
+class Marta:
+    def __init__(self):
+        pass
+
+    def get_message(self, message):
+        return message
+
+    def __str__(self):
+        return "this the brain's Marta"
